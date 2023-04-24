c7653221fd4ca778600e9fd220d162afd2d0626e
Amir Hossein Amanipoor <76044261+Amirdtv@users.noreply.github.com>
2023-04-24 23:00:04 +0330

    ss

---
+++ tamrin 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrin_5
+{
+    internal class Program
+    {
+        private static int test(int x)
+        {
+            double result;
+            result = Math.Pow(x,2) + (2*x) - 4;
+            Console.WriteLine(result);
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" x ra vared konin");
+            int x = Convert.ToInt32(Console.ReadLine());
+            test(x);
+        }
+    }
+}
